d350df3542cf4c39505dea86ced6b00d7badd4cb
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2022-10-07 16:40:42 -0500

    The seven figures of hangman and menu are working

--- hangman.py
+++ hangman.py
@@ -1,80 +1,124 @@
-"""
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
+import random;
+from os import system, name;
 
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
+def list_hangman_attempts(attempt):
+    hangman_figure = [
+        """
+            +---+
+            |   
+            |
+            |
+            |
+            |
+        =========
+        """,
+        """
+            +---+
+            |   |
+            |
+            |
+            |
+            |
+        =========
+        """,
+        """
+            +---+
+            |   |
+            |   O
+            |
+            |
+            |
+        =========
+        """,
+        """
+            +---+
+            |   |
+            |   O 
+            |  /|
+            |
+            |
+        =========
+        """,
+        """
+            +---+
+            |   |
+            |   O
+            |  /|\\
+            |
+            |
+        =========
+        """,
+        """
+            +---+
+            |   |
+            |   O
+            |  /|\\
+            |  /
+            |
+        =========
+        """,
+        """
+            +---+
+            |   |
+            |   O
+            |  /|\\
+            |  / \\
+            |
+        =========
+        """
+    ]
+    return hangman_figure[attempt]
 
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
 
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========
+def welcome_message():
+    return """
+            ██╗░░██╗░█████╗░███╗░░██╗░██████╗░███╗░░░███╗░█████╗░███╗░░██╗
+            ██║░░██║██╔══██╗████╗░██║██╔════╝░████╗░████║██╔══██╗████╗░██║
+            ███████║███████║██╔██╗██║██║░░██╗░██╔████╔██║███████║██╔██╗██║
+            ██╔══██║██╔══██║██║╚████║██║░░╚██╗██║╚██╔╝██║██╔══██║██║╚████║
+            ██║░░██║██║░░██║██║░╚███║╚██████╔╝██║░╚═╝░██║██║░░██║██║░╚███║
+            ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝
+            Welcome to hangman play - You only have seven attempts.
+                Please select an option:
+                1 - Play.
+                2 - Words between 4 and 5 letters. -- Coming soon
+                3 - More than 6 letters. -- Coming soon
+                4 - Restart game.
+                5 - To quit
+    """
 
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
 
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-
-"""
-import random
-import os
+def clearConsole():
+    if name == "nt":
+        system("cls")
+    else:
+        system("clear")
 
 
 def menu():
-    selected_option = int(input("""
-        ██╗░░██╗░█████╗░███╗░░██╗░██████╗░███╗░░░███╗░█████╗░███╗░░██╗
-        ██║░░██║██╔══██╗████╗░██║██╔════╝░████╗░████║██╔══██╗████╗░██║
-        ███████║███████║██╔██╗██║██║░░██╗░██╔████╔██║███████║██╔██╗██║
-        ██╔══██║██╔══██║██║╚████║██║░░╚██╗██║╚██╔╝██║██╔══██║██║╚████║
-        ██║░░██║██║░░██║██║░╚███║╚██████╔╝██║░╚═╝░██║██║░░██║██║░╚███║
-        ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝
-        Welcome to hangman play.
-            Please select an option:
-            1 - Words between 1 and 3 letters.
-            2 - Words between 4 and 5 letters.
-            3 - More than 6 letters.
-    """))
+    running = True
 
+    while running:
+        option = input(welcome_message())
+
+        if (option == '1' or option == '2' or option == '3'):
+            clearConsole()
+            print(list_hangman_attempts(6))
+            print('Iniciamos el proceso limpiando consola')
+
+        elif option == '4':
+            clearConsole()
+            print('Restarting Game')
+
+        elif option == '5':
+            clearConsole()
+            print('Thank you to play this game')
+            running = False
+        
+        else:
+            clearConsole()
+            print('Please choose a correct option')
+        
 
 def run():
     menu()
